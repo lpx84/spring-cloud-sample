c1e38d036f91030f215df95f335446adb607f54a
李鹏翔 <lipengxiang1@xiaomi.com>
2019-06-17 14:50:51 +0800

    add arc config
    Summary: Signed-off-by: 李鹏翔 <lipengxiang1@xiaomi.com>
    
    Differential Revision: https://phabricator.d.xiaomi.net/D159735

---
+++ xiang-consumer/src/main/java/com/xiang/cloud/consumer/base/ICommonEnum.java
@@ -0,0 +1,15 @@
+package com.xiang.cloud.consumer.base;
+
+import lombok.Data;
+
+/**
+ * @author 李鹏翔(lipengxiang1)
+ * @date 2019/6/12
+ **/
+public interface ICommonEnum {
+
+    int getValue();
+
+    String getDescription();
+
+}
